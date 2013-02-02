1224156abe65456f0bca63db8499ecb735f5bba7
Spencer <spencecooley@gmail.com>
2013-02-02 04:05:05 -0600

    initial commit

---
+++ demo1/assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  
+	
+	var conjunctured = [30.264466, -97.728465]
+	
+	var mapBoxLayers = L.tileLayer('http://a.tiles.mapbox.com/v3/spencercooley.map-kqxiouyq/{z}/{x}/{y}.png', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	    maxZoom: 18
+	});
+
+
+	var map = L.map('the_map', {
+		center: conjunctured, 
+		layers: mapBoxLayers,
+		zoom: 15,
+	    maxZoom: 18
+	});
+
+
+});
